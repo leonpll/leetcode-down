b670df02227732fffd3073f556087dc08f5ff317
leonpp <leonpp@aliyun.com>
2020-05-07 15:10:56 +0800

    leetcode572

---
+++ leetcode/src/main/java/org/leonpp/questionBank/tree/LeetCode572.java
@@ -0,0 +1,72 @@
+package org.leonpp.questionBank.tree;
+
+/**
+ * @program: leetcode-down
+ * @description: 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subtree-of-another-tree
+ * @author: LL
+ * @create: 2020-05-07 13:28
+ **/
+public class LeetCode572 {
+
+
+    class TreeNode {
+
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    boolean isSubtree(TreeNode s, TreeNode t) {
+        if (t == null) {
+            return true;
+        }
+        if (s == null) {
+            return false;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t) || isSameTree(s, t);
+
+    }
+
+
+    /**
+     * 判断两棵树是否相同
+     */
+    private boolean isSameTree(TreeNode s, TreeNode t) {
+
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+
+
+    /**
+     * 参考https://leetcode-cn.com/problems/subtree-of-another-tree/solution/java-di-gui-ban-by-kelly2018/
+     */
+}
+
+
